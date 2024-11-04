ba3e4e2e49591d2ca06660783b4255b38b3caaa0
Brian Payne <payne.in.the.brian@gmail.com>
2024-11-04 14:11:30 -0600

    fixed index page

--- frontend/src/routes/Index.jsx
+++ frontend/src/routes/Index.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 export default function Index() {
 	return <main className="container">
 		<img src="/shadowDark.jpg" alt="shadow dark" className="img-fluid mx-auto" />
-		<div className="row justify-content-around mt-5">
+		<div>
+			<h4 className="text-light my-4 mx-2 text-center">Shadow Dark is built on old school D&D. Create a character and track your progress as you play!</h4>
+		</div>
+		<div className="row justify-content-around mt-3">
 			<Link to="/login" className="col-3 btn purple-btn mb-5 fw-bold"> Login</Link>
 			<Link to="/signup" className="col-3 btn purple-btn mb-5 fw-bold"> Signup</Link>
 		</div>
